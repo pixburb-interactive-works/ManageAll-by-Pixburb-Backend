73ce67fcbf38898cfe0d539ca137b39759d8c312
Aswanth Dheenadayalan (MSC Technology India) <aswanth.dheenadayalan@msc.com>
2019-07-18 13:33:41 +0530

    [update]-Inventory Class Added

---
+++ Pixburb.CommonModel/Inventory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pixburb.CommonModel
+{
+    public class Inventory
+    {
+        public int Id { get; set; }
+        public int Category { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Vendor { get; set; }
+        public string Location { get; set; }
+        public string BatchCode { get; set; }
+        public decimal? Price { get; set; }
+        public bool Featured { get; set; }
+        public string Tags { get; set; }
+        public decimal? Stock { get; set; }
+        public double? LimitPerUser { get; set; }
+        public DateTime? StockDate { get; set; }
+        public DateTime? ValidTo { get; set; }
+        public string Image { get; set; }
+        public List<Gallery> Gallery { get; set; } = new List<Gallery>();
+        public string Status { get; set; }
+    }
+}
