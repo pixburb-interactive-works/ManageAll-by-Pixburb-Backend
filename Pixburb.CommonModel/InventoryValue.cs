42c4191e3060cbc972f49b755bf27a7f014469b5
Aswanth D <aswanth.dheenadayalan@hotmail.com>
2019-07-23 23:08:24 +0530

    [update]

---
+++ Pixburb.CommonModel/InventoryValue.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pixburb.CommonModel
+{
+    public class InventoryValue
+    {
+        public int Id { get; set; }
+        public Category Category { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Vendor { get; set; }
+        public string Location { get; set; }
+        public string BatchCode { get; set; }
+        public decimal? Price { get; set; }
+        public bool Featured { get; set; }
+        public string Tags { get; set; }
+        public decimal? Stock { get; set; }
+        public double? LimitPerUser { get; set; }
+        public DateTime? StockDate { get; set; }
+        public DateTime? ValidTo { get; set; }
+        public FileAttachment File { get; set; }
+        public List<FileAttachment> Files { get; set; } = new List<FileAttachment>();
+        public string Status { get; set; }
+        public string RecordStatus { get; set; }
+    }
+}
