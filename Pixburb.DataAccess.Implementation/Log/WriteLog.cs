706b0cf3fbbc6fff23add9cee51bf4e975d0e571
Aswanth Dheenadayalan (MSC Technology India) <aswanth.dheenadayalan@msc.com>
2019-07-24 10:06:47 +0530

    [update]-Log

---
+++ Pixburb.DataAccess.Implementation/Log/WriteLog.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pixburb.DataAccess.Implementation.Log
+{
+    public class WriteLog
+    {
+        public Task WriteDbLogAsync(string storedProcedure, SqlParameterCollection parameters)
+        {
+            try
+            {
+                Dictionary<string, object> dbParameters = new Dictionary<string, object>();
+                dbParameters.Add("DBCommand : ", storedProcedure);
+                for (int i =0; i < parameters.Count; i++)
+                {
+                    dbParameters.Add(parameters[i].ParameterName,parameters[i].Value);
+                }
+                
+                LoggingManager.WriteDBLog(dbParameters);
+                return Task.FromResult<bool>(true);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+    }
+}
