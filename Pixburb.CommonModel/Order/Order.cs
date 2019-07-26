e86c7fe9c6cb3f43ce003ffaf3ca1c461ae3a514
Aswanth Dheenadayalan (MSC Technology India) <aswanth.dheenadayalan@msc.com>
2019-07-26 12:08:12 +0530

    [update]-Model Categorised

---
+++ Pixburb.CommonModel/Order/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pixburb.CommonModel
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string OrderId { get; set; }
+        public string Customername { get; set; }
+        public string Payment { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime? OrderDate { get; set; }
+        public string PaymentStatus { get; set; }
+    }
+}
